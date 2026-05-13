f47b6e2ea4909f2332d0f3ed0c21c2f9ac51e97e
Apisathan Sivananthanathan <88615588+apisathan2000@users.noreply.github.com>
2026-05-13 17:57:37 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        groupedList = []
+        for x in strs:
+            key = "".join(sorted(x))
+            if key in anagrams:
+                anagrams[key].append(x)
+            else:
+                anagrams[key] = [x]
+
+        return list(anagrams.values())
+
+# from typing import List
+# from collections import Counter
+
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         groupedLists = []
+#         anagrams = {}
+
+#         for x in strs:
+#             key = tuple(sorted(Counter(x).items()))
+#             if key in anagrams:
+#                 anagrams[key].append(x)
+#             else:
+
+#                 anagrams[key] = [x]
+
+#         groupedLists = anagrams.values()
+
+#         print(groupedLists)
+
+        
